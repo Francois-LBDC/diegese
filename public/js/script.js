f36005cd261d71b60c8de76f9ba01731e8c8ceae
Francois-LBDC <francois.leblancdecernex@kedgebs.com>
2021-01-23 15:53:00 +0100

    seulement texte, essaies de changer background color selon curseur

--- public/js/script.js
+++ public/js/script.js
@@ -5,13 +5,14 @@
 window.onmousemove = function(e) {
     
     if (e.target.classList.contains('dot')) {
-    		//	Content of dot which is hovered
+        
+        //	Content of dot which is hovered
         var $target = e.target.nextElementSibling;
         
         //	Only work with hovered target
         //	Add visible class hovered elem to distinguish
         if (!$target.classList.contains('visible')) {
-        		$target.classList.add('visible');
+            $target.classList.add('visible');
         } else {
             //	Get viewport offset of tooltip element
             var offset = $target.parentElement.getBoundingClientRect();
@@ -19,12 +20,12 @@ window.onmousemove = function(e) {
             var tipDist = 80;
             //	Calc and set new tooltip location
             $target.style.top = (e.clientY - offset.top - tipDist) + 'px';
-            console.log('e' , e.clientY);
-            console.log($target.style.top);
+            //console.log('e' , e.clientY);
+            //console.log($target.style.top);
             $target.style.left = (e.clientX - offset.left ) + 'px';
         }
     } else {
-    		//	Remove visible class
+            //	Remove visible class
         var content = document.getElementsByClassName('content');
         for (var i = 0; i < content.length; i++) {
         		content[i].classList.remove('visible');
