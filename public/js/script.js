df03af2a986f73bf3a0671c878e3bddbec57b200
Francois-LBDC <francois.leblancdecernex@kedgebs.com>
2021-01-31 19:57:15 +0100

    ajout flèche, couleur, nettoyage code

--- public/js/script.js
+++ public/js/script.js
@@ -14,18 +14,13 @@ window.onmousemove = function(e) {
         if (!$target.classList.contains('visible')) {
             $target.classList.add('visible');
         } else {
-            //	Get viewport offset of tooltip element
             var offset = $target.parentElement.getBoundingClientRect();
-            //	Tooltip distance from mouse(px)
             var tipDist = 80;
-            //	Calc and set new tooltip location
             $target.style.top = (e.clientY - offset.top - tipDist) + 'px';
-            //console.log('e' , e.clientY);
-            //console.log($target.style.top);
             $target.style.left = (e.clientX - offset.left ) + 'px';
           
-            //WORKS 
-
+     
+            // vérifie si c'est le cadre diégèse2 ou pas et applique le bon style
             if (e.target.classList.contains('diegese2')) {
            e.target.style.background = `
            radial-gradient(circle at 70%, #000000 0px,transparent 40px),
@@ -33,9 +28,6 @@ window.onmousemove = function(e) {
             } else {
                 e.target.style.background = `radial-gradient(circle at ${e.clientX - offset.left}px ${e.clientY - offset.top -40}px, #ff00ff 20px,#eeeeee 200px)`
             }
-
-           console.log(e.clientY - offset.top);
-            //
         }
     } else {
             //	Remove visible class
